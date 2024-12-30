9071ca91356fe9780484fbeb1bb40f178c0bb8c4
Ekaterina Gontsova <blizkot@gmail.com>
2024-12-30 12:11:06 +0300

    Add files

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js', 
+    output: {
+        path: path.resolve(__dirname, 'dist'), 
+        filename: 'bundle.js', 
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/, 
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                ],
+            },
+            {
+                test: /\.js$/, 
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html', 
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'style.css', 
+        }),
+    ],
+}
